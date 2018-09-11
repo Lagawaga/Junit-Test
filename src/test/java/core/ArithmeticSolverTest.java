dbd29a2c6cf2314c46061212c29ab58ec757f5d7
Lagawaga <kamronjahani@gmail.com>
2018-09-11 17:57:25 -0400

    Some simple functions to practice Junit test class

--- src/test/java/core/ArithmeticSolverTest.java
+++ src/test/java/core/ArithmeticSolverTest.java
@@ -6,32 +6,32 @@ public class ArithmeticSolverTest extends TestCase{
 	
 	public void testAdd() {
 		ArithmeticSolver solver = new ArithmeticSolver();
-		assertEquals(4, solver.add(2,2));
-		assertEquals(69, solver.add(34,35));
+		assertEquals(4.0, solver.add(2,2));
+		
 		
 	}
 	
 	public void testSubtract() {
 		ArithmeticSolver solver = new ArithmeticSolver();
-		assertEquals(0, solver.subtract(2,2));
-		assertEquals(5, solver.subtract(10,5));
+		assertEquals(2.0, solver.subtract(4,2));
+		
 	}
 	
 	public void testMultiply() {
 		ArithmeticSolver solver = new ArithmeticSolver();
-		assertEquals(4, solver.multiply(2,2));
-		assertEquals(54, solver.multiply(9,6));
+		assertEquals(4.0, solver.multiply(2,2));
+		
 		
 	}
 
 	public void testDivide() {
 		ArithmeticSolver solver = new ArithmeticSolver();
-		assertEquals(0, solver.divide(2,2));
+		assertEquals(1.0, solver.divide(2,2));
 		
 	}
 	public void testModulus() {
 		ArithmeticSolver solver = new ArithmeticSolver();
-		assertEquals(1, solver.modulus(5,2));
+		assertEquals(1.0, solver.modulus(5,2));
 		
 	}
 }
